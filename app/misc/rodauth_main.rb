1770bd8c39001cdb9cd3cfe182cd38f51f128acf
kimura <hiroshikimura.dot.com@gmail.com>
2023-11-18 18:09:43 +0900

    rodauthのdatabase driver誤りを訂正

--- app/misc/rodauth_main.rb
+++ app/misc/rodauth_main.rb
@@ -15,7 +15,7 @@ class RodauthMain < Rodauth::Rails::Auth
 
     # ==> General
     # Initialize Sequel and have it reuse Active Record's database connection.
-    db Sequel.sqlite(extensions: :activerecord_connection, keep_reference: false)
+    db Sequel.postgres(extensions: :activerecord_connection, keep_reference: false)
 
     # Change prefix of table and foreign key column names from default "account"
     # accounts_table :users
